2b1cba7733ffb6b8366725a79c3fc20678c72242
Ivan Miano <44769075+ikgmiano@users.noreply.github.com>
2026-05-13 19:53:47 +0800

    add background color randomizer

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const counter = document.getElementById("counter");
 const addButton = document.getElementById("button-add");
 const subButton = document.getElementById("button-sub");
+const randButton = document.getElementById("button-bgc");
+const body = document.getElementsByTagName("body")[0];
 
 addButton.addEventListener("click", () => {
     counter.textContent++;
@@ -9,4 +11,9 @@ addButton.addEventListener("click", () => {
 subButton.addEventListener("click", () => {
     if (counter.textContent == 0) { return; }
     counter.textContent--;
+});
+
+randButton.addEventListener("click", () => {
+    const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16).padStart(6, "0");
+    body.style.backgroundColor = randomColor;
 });
